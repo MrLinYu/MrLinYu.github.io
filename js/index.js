bde767e090a0af8cad876cd5623babe66c61da84
MrLinYu <34209717+MrLinYu@users.noreply.github.com>
2018-04-22 23:16:55 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,5 @@ console.log("you are last")
 var a=11;
 console.log(a+10)
 console.log(1234)
-<<<<<<< HEAD
 console.log("second");
-=======
->>>>>>> c7b7d7edd38c0ed1b6cd0542cc5e888f5b37f458
+console.log('second1212');
