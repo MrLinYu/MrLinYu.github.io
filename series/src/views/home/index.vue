22c5d6007a8e15aa53b2b209fc5a8aeed2bad588
张廷江 <tj.zhang@boringkiller.cn>
2020-04-20 14:48:39 +0800

    dsd

---
+++ series/src/views/home/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>这是body</h1>
+  </div>
+</template>
+<script lang='ts'>
+import { Component, Prop, Vue } from 'vue-property-decorator'
+@Component
+export default class Home extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+<style scoped lang='scss'></style>
---
+++ series/src/views/slide-nav/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="header-container">
+    <ul>
+      <li>这是标题一</li>
+      <li>这是标题一</li>
+      <li>这是标题一</li>
+    </ul>
+  </div>
+</template>
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class HeaderNav extends Vue {
+
+}
+</script>
+<style scoped lang='scss'>
+.header-container {
+  width: 200px;
+  height: 100%;
+  border-right: 1px solid #dfdfdf;
+  background: #ffff;
+}
+</style>
